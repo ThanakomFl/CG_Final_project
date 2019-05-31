a3c4dc4ba45c42c90ed5b2cc9b42b0e185fe90de
ThanakomFl <30922226+ThanakomFl@users.noreply.github.com>
2019-06-01 01:00:09 +0700

    Add files via upload

---
+++ Project1/myTexture.h
@@ -0,0 +1,6 @@
+void drawTexPlate(float s);
+void drawTexPlate(float s, int repeatNumber);
+void drawTexCube(float s);
+void drawTexSphere(float radius, int nSlice, int nStack);
+void drawTexCylinder(float rBottom, float rTop, float height, int nSlice, int nStack);
+void drawTexTorus(float radius1, float radius2, int nSide, int nRing);
